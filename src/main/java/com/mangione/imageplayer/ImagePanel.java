43d4d42b8d0767f22d811539ffd8c4a49ae926f2
carmine <carmine@mangione.com>
2017-10-08 16:16:08 -0700

    Made player able to do animated gifs.

--- src/main/java/com/mangione/imageplayer/ImagePanel.java
+++ src/main/java/com/mangione/imageplayer/ImagePanel.java
@@ -1,137 +1,101 @@
 package com.mangione.imageplayer;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
-import java.io.IOException;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 
 public class ImagePanel extends JPanel {
     private Dimension imageSize;
-    private Image currentImage;
     private String currentImageFile;
+    private final JLabel jLabel;
 
 
-    public ImagePanel() {
+    ImagePanel(String currentImageFile)  {
+        setOpaque(true);
+        setBackground(Color.DARK_GRAY);
+        this.currentImageFile = currentImageFile;
+        setLayout(new BorderLayout());
+
+        jLabel = new JLabel(new ImageIcon(currentImageFile));
+        jLabel.setOpaque(false);
+        add(jLabel, BorderLayout.CENTER);
+
+        addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                SwingUtilities.invokeLater(() -> {
+                    jLabel.setIcon(getCurrentImage());
+                    repaint();
+                });
+            }
+        });
+
+    }
+
+    private ImageIcon getCurrentImage() {
+        final ImageIcon imageIcon = new ImageIcon(currentImageFile);
+        final QuadDimension quadDimension = new QuadDimension(imageIcon.getImage());
+        return new ImageIcon(imageIcon.getImage().getScaledInstance(quadDimension.size.width,
+               quadDimension.size.height, Image.SCALE_REPLICATE));
     }
 
     public synchronized void setImage(final String imageFileName) {
         currentImageFile = imageFileName;
-        if (currentImage != null) {
-            currentImage = null;
-        }
-
         SwingUtilities.invokeLater(() -> {
-            invalidate();
+            jLabel.setIcon(getCurrentImage());
             repaint();
         });
-//        new Thread() {
-//            @Override
-//            public void run() {
-//                if (currentImageFile == null) {
-//                    currentImageFile = imageFileName;
-//                }
-//                numberOfSteps = 0;
-//                fadeOut = true;
-//                animating = true;
-//                currentAlpha = 1.0f;
-//                while (animating) {
-//                    try {
-//                        if (!fadeOut) {
-//                            if (numberOfSteps == NUMBER_OF_STEPS) {
-//                                currentAlpha = 1.0f;
-//                                animating = false;
-//                            } else {
-//                                currentAlpha += 0.95f / NUMBER_OF_STEPS;
-//                                numberOfSteps++;
-//                            }
-//
-//                        } else {
-//                            if (numberOfSteps == NUMBER_OF_STEPS) {
-//                                fadeOut = false;
-//                                numberOfSteps = 0;
-//                                currentAlpha = 0.0f;
-//                                invalidate();
-//                                repaint();
-//                                Thread.sleep(500);
-//                                currentImageFile = imageFileName;
-//                                currentImage = null;
-//                            } else {
-//                                numberOfSteps++;
-//                                currentAlpha -= 0.75f / NUMBER_OF_STEPS;
-//                            }
-//
-//                        }
-//
-//                        currentAlpha = Math.min(1.0f, currentAlpha);
-//                        currentAlpha = Math.max(0f, currentAlpha);
-//                        SwingUtilities.invokeLater(new Runnable() {
-//                            public void run() {
-//                                invalidate();
-//                                repaint();
-//                            }
-//                        });
-//                        Thread.sleep(50);
-//                    } catch (InterruptedException e) {
-//                        // noop
-//                    }
-//                }
-
-//            }
-//        }.start();
-
 
     }
 
+    @Override
     public void paint(Graphics g) {
-        Graphics2D g2d = (Graphics2D) g;
-        Dimension panelSize = getSize();
-        if (currentImage == null) {
-            try {
-                currentImage = ImageIO.read(new File(currentImageFile));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        double panelWidth = panelSize.getWidth();
-        double panelHeight = panelSize.getHeight();
-
-        imageSize = new Dimension(currentImage.getWidth(null),
-                currentImage.getHeight(null));
-        double imageWidth = currentImage.getWidth(null) * 2;
-        double imageHeight = currentImage.getHeight(null) * 2;
-
-        double adjustedImageWidth;
-        double adjustedImageHeight;
-
-        double aspectRatioOfImage = imageWidth / imageHeight;
-
-        if (imageWidth <= panelWidth && imageHeight < panelHeight) {
-            adjustedImageWidth = imageWidth;
-            adjustedImageHeight = imageHeight;
-        } else {
-            adjustedImageWidth = Math.min(panelWidth, imageWidth * 2);
-            adjustedImageHeight = panelWidth / aspectRatioOfImage;
-
-            if (adjustedImageHeight >= panelHeight) {
-                adjustedImageHeight = Math.min(panelHeight, imageHeight * 2);
-                adjustedImageWidth = panelHeight * aspectRatioOfImage;
-            }
-
-        }
-
-        int imageStartX = (int) ((panelWidth - adjustedImageWidth)) / 2;
-        int imageStartY = (int) ((panelHeight - adjustedImageHeight)) / 2;
         super.paint(g);
-        g2d.setColor(Color.darkGray);
-        g2d.fillRect(0, 0, panelSize.width, panelSize.height);
-        g2d.drawImage(currentImage, imageStartX, imageStartY, (int) adjustedImageWidth, (int) adjustedImageHeight, null);
-
     }
 
     public Dimension getMaximumSize() {
         return imageSize;
     }
+
+    private class QuadDimension {
+        private final Dimension size;
+
+        QuadDimension(Image currentImage) {
+            Dimension panelSize = getSize();
+
+            double panelWidth = panelSize.getWidth();
+            double panelHeight = panelSize.getHeight();
+
+            imageSize = new Dimension(currentImage.getWidth(null),
+                    currentImage.getHeight(null));
+            double imageWidth = currentImage.getWidth(null) * 2;
+            double imageHeight = currentImage.getHeight(null) * 2;
+
+            double adjustedImageWidth;
+            double adjustedImageHeight;
+
+            double aspectRatioOfImage = imageWidth / imageHeight;
+
+            if (imageWidth <= panelWidth && imageHeight < panelHeight) {
+                adjustedImageWidth = imageWidth;
+                adjustedImageHeight = imageHeight;
+            } else {
+                adjustedImageWidth = Math.min(panelWidth, imageWidth * 2);
+                adjustedImageHeight = panelWidth / aspectRatioOfImage;
+
+                if (adjustedImageHeight >= panelHeight) {
+                    adjustedImageHeight = Math.min(panelHeight, imageHeight * 2);
+                    adjustedImageWidth = panelHeight * aspectRatioOfImage;
+                }
+
+            }
+            size = new Dimension((int) adjustedImageWidth, (int) adjustedImageHeight);
+
+        }
+
+    }
+
 
 }
--- src/main/java/com/mangione/imageplayer/ImagePanelController.java
+++ src/main/java/com/mangione/imageplayer/ImagePanelController.java
@@ -11,16 +11,29 @@ import javax.swing.*;
 public class ImagePanelController {
     private final ImagePanel imagePanel;
 
-    public ImagePanelController(String initialImage) {
-        imagePanel = new ImagePanel();
-        imagePanel.setImage(initialImage);
+//    public static void main(String[] args) throws InvocationTargetException, InterruptedException {
+//        SwingUtilities.invokeLater(()->{
+//            final ImagePanelController controller = new ImagePanelController("/Users/carmine/Pictures/internet/allpics/1sz13pwa1qmbd9oo4_500.gif");
+//            JFrame frame = new JFrame();
+//            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//            final JComponent imagePanel = controller.getImagePanel();
+//            imagePanel.setPreferredSize(new Dimension(800, 600));
+//            frame.getContentPane().add(imagePanel, BorderLayout.CENTER);
+//            frame.pack();
+//      frame.setVisible(true);
+//
+//        });
+//    }
+
+    ImagePanelController(String initialImage) {
+        imagePanel = new ImagePanel(initialImage);
     }
 
     public void setImage(String newImage) {
         imagePanel.setImage(newImage);
     }
 
-    public JComponent getImagePanel() {
+    JComponent getImagePanel() {
         return imagePanel;
     }
 }

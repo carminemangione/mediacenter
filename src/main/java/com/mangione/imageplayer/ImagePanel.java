b25dc28ab02ace8a736ed4e302b69f3d5e75775b
carmine <carmine@mangione.com>
2020-12-31 14:10:20 -0800

    Revert "Made player able to do animated gifs."
    This reverts commit 35a45ed4

--- src/main/java/com/mangione/imageplayer/ImagePanel.java
+++ src/main/java/com/mangione/imageplayer/ImagePanel.java
@@ -1,132 +1,101 @@
 package com.mangione.imageplayer;
 
-import com.luciad.imageio.webp.WebPReadParam;
-
-import javax.imageio.ImageIO;
-import javax.imageio.ImageReader;
-import javax.imageio.stream.FileImageInputStream;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
 
 public class ImagePanel extends JPanel {
-	private Dimension imageSize;
-	private String currentImageFile;
-	private final JLabel jLabel;
+    private Dimension imageSize;
+    private String currentImageFile;
+    private final JLabel jLabel;
 
 
-	ImagePanel(String currentImageFile) {
-		setOpaque(true);
-		setBackground(Color.DARK_GRAY);
-		this.currentImageFile = currentImageFile;
-		setLayout(new BorderLayout());
+    ImagePanel(String currentImageFile)  {
+        setOpaque(true);
+        setBackground(Color.DARK_GRAY);
+        this.currentImageFile = currentImageFile;
+        setLayout(new BorderLayout());
 
-		jLabel = new JLabel(new ImageIcon(currentImageFile));
-		jLabel.setOpaque(false);
-		add(jLabel, BorderLayout.CENTER);
-		addComponentListener(new ComponentAdapter() {
-			@Override
-			public void componentResized(ComponentEvent e) {
-				SwingUtilities.invokeLater(() -> {
-					ImageIcon currentImage = getCurrentImage();
-					if (currentImage != null) {
-						jLabel.setIcon(currentImage);
-						repaint();
-					}
-				});
-			}
-		});
+        jLabel = new JLabel(new ImageIcon(currentImageFile));
+        jLabel.setOpaque(false);
+        add(jLabel, BorderLayout.CENTER);
 
-	}
+        addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                SwingUtilities.invokeLater(() -> {
+                    jLabel.setIcon(getCurrentImage());
+                    repaint();
+                });
+            }
+        });
 
-	private ImageIcon getCurrentImage() {
-		final ImageIcon imageIcon = loadImage();
-		if (imageIcon != null) {
-			final QuadDimension quadDimension = new QuadDimension(imageIcon.getImage());
-			return new ImageIcon(imageIcon.getImage().getScaledInstance(quadDimension.size.width,
-					quadDimension.size.height, Image.SCALE_REPLICATE));
-		}
-		return null;
-	}
+    }
 
-	private ImageIcon loadImage() {
-		ImageIcon imageIcon = null;
-		final String[] pieces = currentImageFile.split("\\.");
-		ImageReader reader = ImageIO.getImageReadersBySuffix(pieces[pieces.length - 1]).next();
-		WebPReadParam readParam = new WebPReadParam();
-		readParam.setBypassFiltering(true);
-		try {
-			reader.setInput(new FileImageInputStream(new File(currentImageFile)));
-			BufferedImage image = reader.read(0, readParam);
-			imageIcon = new ImageIcon(image);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return imageIcon;
-	}
+    private ImageIcon getCurrentImage() {
+        final ImageIcon imageIcon = new ImageIcon(currentImageFile);
+        final QuadDimension quadDimension = new QuadDimension(imageIcon.getImage());
+        return new ImageIcon(imageIcon.getImage().getScaledInstance(quadDimension.size.width,
+               quadDimension.size.height, Image.SCALE_REPLICATE));
+    }
 
-	public synchronized void setImage(final String imageFileName) {
-		currentImageFile = imageFileName;
-		try {
-			SwingUtilities.invokeLater(() -> {
-				jLabel.setIcon(getCurrentImage());
-				repaint();
-			});
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public synchronized void setImage(final String imageFileName) {
+        currentImageFile = imageFileName;
+        SwingUtilities.invokeLater(() -> {
+            jLabel.setIcon(getCurrentImage());
+            repaint();
+        });
 
-	@Override
-	public void paint(Graphics g) {
-		super.paint(g);
-	}
+    }
 
-	public Dimension getMaximumSize() {
-		return imageSize;
-	}
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+    }
 
-	private class QuadDimension {
-		private final Dimension size;
+    public Dimension getMaximumSize() {
+        return imageSize;
+    }
 
-		QuadDimension(Image currentImage) {
-			Dimension panelSize = getSize();
+    private class QuadDimension {
+        private final Dimension size;
 
-			double panelWidth = panelSize.getWidth();
-			double panelHeight = panelSize.getHeight();
+        QuadDimension(Image currentImage) {
+            Dimension panelSize = getSize();
 
-			imageSize = new Dimension(currentImage.getWidth(null),
-					currentImage.getHeight(null));
-			double imageWidth = currentImage.getWidth(null) * 2;
-			double imageHeight = currentImage.getHeight(null) * 2;
+            double panelWidth = panelSize.getWidth();
+            double panelHeight = panelSize.getHeight();
 
-			double adjustedImageWidth;
-			double adjustedImageHeight;
+            imageSize = new Dimension(currentImage.getWidth(null),
+                    currentImage.getHeight(null));
+            double imageWidth = currentImage.getWidth(null) * 2;
+            double imageHeight = currentImage.getHeight(null) * 2;
 
-			double aspectRatioOfImage = imageWidth / imageHeight;
+            double adjustedImageWidth;
+            double adjustedImageHeight;
 
-			if (imageWidth <= panelWidth && imageHeight < panelHeight) {
-				adjustedImageWidth = imageWidth;
-				adjustedImageHeight = imageHeight;
-			} else {
-				adjustedImageWidth = Math.min(panelWidth, imageWidth * 2);
-				adjustedImageHeight = panelWidth / aspectRatioOfImage;
+            double aspectRatioOfImage = imageWidth / imageHeight;
 
-				if (adjustedImageHeight >= panelHeight) {
-					adjustedImageHeight = Math.min(panelHeight, imageHeight * 2);
-					adjustedImageWidth = panelHeight * aspectRatioOfImage;
-				}
+            if (imageWidth <= panelWidth && imageHeight < panelHeight) {
+                adjustedImageWidth = imageWidth;
+                adjustedImageHeight = imageHeight;
+            } else {
+                adjustedImageWidth = Math.min(panelWidth, imageWidth * 2);
+                adjustedImageHeight = panelWidth / aspectRatioOfImage;
 
-			}
-			size = new Dimension((int) adjustedImageWidth, (int) adjustedImageHeight);
-		}
+                if (adjustedImageHeight >= panelHeight) {
+                    adjustedImageHeight = Math.min(panelHeight, imageHeight * 2);
+                    adjustedImageWidth = panelHeight * aspectRatioOfImage;
+                }
 
-	}
+            }
+            size = new Dimension((int) adjustedImageWidth, (int) adjustedImageHeight);
+
+        }
+
+    }
 
 
 }

2b43caaaebf0654580b05b9871c089f8b4c920af
carminemangione <carmine@mangione.com>
2022-09-06 15:25:36 -0700

    Added MP4 support

--- src/test/java/com/mangione/imageplayer/Video.java
+++ src/test/java/com/mangione/imageplayer/Video.java
@@ -1,15 +1,6 @@
 package com.mangione.imageplayer;
 
 import javafx.application.Platform;
-import javafx.beans.binding.Bindings;
-import javafx.beans.property.DoubleProperty;
-import javafx.embed.swing.JFXPanel;
-import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
-import javafx.scene.paint.Color;
 
 import javax.swing.*;
 import java.awt.*;
@@ -19,7 +10,7 @@ public class Video {
 	private String Dir = System.getProperty("user.dir");
 	private final JFrame jframe;
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		new Video();
 	}
 
@@ -35,39 +26,8 @@ public class Video {
 	}
 
 	public void start() {
-		JFXPanel jfxPanel = new JFXPanel();
-		//Converts media to string URL
-		Media media = new Media(new File(
-				"/Users/carmine/projects/imageplayer/src/test/resources/MetallicLongBat-mobile.mp4").toURI().toString());
-		MediaPlayer player = new MediaPlayer(media);
-		MediaView viewer = new MediaView(player);
-
-		//change width and height to fit video
-		viewer.setPreserveRatio(true);
-
-		StackPane root = new StackPane();
-		root.getChildren().add(viewer);
-
-		Platform.runLater(() -> {
-			DoubleProperty width = viewer.fitWidthProperty();
-			DoubleProperty height = viewer.fitHeightProperty();
-			width.bind(Bindings.selectDouble(viewer.sceneProperty(), "width"));
-			height.bind(Bindings.selectDouble(viewer.sceneProperty(), "height"));
-			viewer.setPreserveRatio(true);
-			//set the Scene
-			Scene scenes = new Scene(root, 500, 500, Color.BLACK);
-			player.play();
-			player.setOnEndOfMedia(new Runnable() {
-				@Override
-				public void run() {
-					player.stop();
-				}
-			});
-
-			jfxPanel.setScene(scenes);
-			jfxPanel.setPreferredSize(new Dimension(400, 400));
-			jframe.add(jfxPanel, BorderLayout.CENTER);
-		});
-
+		final File file = new File("/Users/carmine/projects/imageplayer/src/test/resources/ThirstyDrearySwan-mobile.mp4");
+		final VideoPanel videoPanel = new VideoPanel(file);
+		SwingUtilities.invokeLater(() -> jframe.add(videoPanel.getPanel(), BorderLayout.CENTER));
 	}
 }
